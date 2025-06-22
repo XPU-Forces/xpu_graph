39d1d1bc87cedce22aa5e692d6e74707126dfcd0
yfchen-byted <chenyifan.42@bytedance.com>
2025-06-22 15:18:28 +0800

    fix(ci): dummy test in npu targets

--- xpu_graph/backends/npu.py
+++ xpu_graph/backends/npu.py
@@ -1,12 +1,11 @@
 from typing import Dict
+
 import torch
 
 
-def npu_compile(
-    module: torch.nn.Module, inputs, config_dict: Dict, **kwargs
-) -> torch.nn.Module:
-    from torch._inductor.compile_fx import compile_fx, compile_fx_inner
+def npu_compile(module: torch.nn.Module, inputs, config_dict: Dict, **kwargs) -> torch.nn.Module:
     from torch import _TorchCompileInductorWrapper
+    from torch._inductor.compile_fx import compile_fx, compile_fx_inner
 
     # default means None. In torch, _TorchCompileInductorWrapper's apply_mode just passes.
     mode = config_dict.get("mode", "default")
@@ -15,5 +14,5 @@ def npu_compile(
     inductor_backend = _TorchCompileInductorWrapper(mode, options, dynamic)
     with torch._inductor.config.patch(inductor_backend.config):
         compiled_func = compile_fx_inner(module, inputs, **kwargs)
-
+    # DUMMY
     return compiled_func
