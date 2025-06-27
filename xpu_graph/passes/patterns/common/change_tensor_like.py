44157d3c7679ddde31e0a8037442ee1b3886bcb3
yfchen-byted <chenyifan.42@bytedance.com>
2025-06-27 14:58:44 +0800

    refactor(CombineWhereCat): refactor slice_where_cat pattern into combine_where_cat * combine where and cat does not require the input to be slices * change it to common patterns

--- xpu_graph/passes/patterns/common/change_tensor_like.py
+++ xpu_graph/passes/patterns/common/change_tensor_like.py
@@ -1,6 +1,7 @@
 import torch
 import torch.fx as fx
 from torch import SymInt
+
 from xpu_graph.fx_utils import FxStage
 from xpu_graph.passes.patterns.pattern import Pattern
 
@@ -14,26 +15,21 @@ class ChangeTensorLike(Pattern):
             torch.ops.aten.ones_like.default: torch.ops.aten.ones.default,
             torch.ops.aten.zeros_like.default: torch.ops.aten.zeros.default,
         }
-        candidates = [
-            node
-            for node in gm.graph.nodes
-            if node.op == "call_function" and node.target in tensor_like_map
-        ]
+        candidates = [node for node in gm.graph.nodes if node.op == "call_function" and node.target in tensor_like_map]
 
         for like in candidates:
             if any([isinstance(s, SymInt) for s in like.meta["val"].shape]):
                 # FIXME: use shape env to get the real shape
                 continue
             changed = True
+            kwargs = {"dtype": like.meta["val"].dtype, "device": like.meta["val"].device}
+            if "pin_memory" in like.kwargs:
+                kwargs["pin_memory"] = like.kwargs["pin_memory"]
             with gm.graph.inserting_before(like):
                 tensor = gm.graph.call_function(
                     tensor_like_map[like.target],
                     args=(list(like.meta["val"].shape),),
-                    kwargs={
-                        "dtype": like.meta["val"].dtype,
-                        "device": like.meta["val"].device,
-                        "pin_memory": like.kwargs["pin_memory"],
-                    },
+                    kwargs=kwargs,
                 )
             like.replace_all_uses_with(tensor)
             gm.graph.erase_node(like)
