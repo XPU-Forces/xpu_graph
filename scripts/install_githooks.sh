13e79d081d3f9bf5fbe154d3959696f35fc81927
wangmingfa@bytedance.com <wangmingfa>
2025-07-02 11:23:22 +0800

    fix(ci): lint_check run on ubuntu-latest

--- scripts/install_githooks.sh
+++ scripts/install_githooks.sh
@@ -1,12 +1,5 @@
 #! /bin/bash
 function init_git_hooks() {
-  export http_proxy=http://sys-proxy-rd-relay.byted.org:3128
-  export https_proxy=http://sys-proxy-rd-relay.byted.org:3128
-  export ftp_proxy=http://sys-proxy-rd-relay.byted.org:3128
-  export no_proxy=.byted.org
-  export PATH=$PATH:$HOME/.local/bin
-  python3 -m pip config --global set global.break-system-packages true
-  python3 -m pip install --no-cache-dir pip --upgrade
   python3 -m pip config set global.disable-pip-version-check true
   [[ $(python3 -m pip list | grep -i pre-commit) ]] || [[ $(python3 -m pip install pre-commit) ]]
   [[ $(pre-commit install -f -t pre-commit -c .pre-commit-config.yaml) ]] && echo "initialize git pre-commit hooks successfully!"
