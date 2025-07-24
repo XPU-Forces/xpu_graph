d87eed74697809e7251e6e666990cc28d07bc012
liuyuan.leo <liuyuan.leo@bytedance.com>
2025-07-24 18:00:02 +0800

    refactor(xpu_graph/passes/patterns/common): refactore fold_add with new pattern matcher.

--- xpu_graph/passes/patterns/common/fold_add.py
+++ xpu_graph/passes/patterns/common/fold_add.py
@@ -1,9 +1,16 @@
 import torch
 import torch.fx as fx
 
-from xpu_graph.passes.patterns.pattern import Pattern
 from xpu_graph.fx_utils import FxStage
+from xpu_graph.passes.patterns.pattern import Pattern
 from xpu_graph.passes.patterns.utils.check_ops import is_zero_like
+from xpu_graph.passes.patterns.utils.pattern_matcher import (
+    AnyNode,
+    AtenAdd,
+    ATenZeroLike,
+    LiteralNode,
+    NodeCapture,
+)
 
 
 class FoldAdd0(Pattern):
@@ -20,37 +27,26 @@ class FoldAdd0(Pattern):
 
     def process(self, gm: fx.GraphModule):
         changed = False
-        add_tup = (
-            torch.ops.aten.add.Tensor,
-            torch.ops.aten.add.Scalar,
+
+        add_node_cap = NodeCapture()
+        target_node_cap = NodeCapture()
+
+        zeros_like_pattern = ATenZeroLike(AnyNode()) | LiteralNode(0) | LiteralNode(0.0)
+        pattern = AtenAdd(AnyNode(capture=target_node_cap), zeros_like_pattern, capture=add_node_cap) | AtenAdd(
+            zeros_like_pattern, AnyNode(capture=target_node_cap), capture=add_node_cap
         )
-        candidates = [
-            node
-            for node in gm.graph.nodes
-            if node.op == "call_function" and node.target in add_tup
-        ]
 
-        for add in candidates:
-            inp0 = add.args[0]
-            inp1 = add.args[1]
-            target_val = None
-            is_match = False
-            if is_zero_like(inp0):
-                is_match = True
-                target_val = inp1
-            elif is_zero_like(inp1):
-                is_match = True
-                target_val = inp0
-
-            if is_match:
+        changed = False
+        for node in reversed(gm.graph.nodes):
+            if pattern.match(node):
                 changed = True
-                with gm.graph.inserting_before(add):
+                with gm.graph.inserting_before(add_node_cap.node):
                     from xpu_graph.passes.patterns.utils.get_binary_fold_result import (
                         get_binary_fold_result,
                     )
 
-                    fold_res = get_binary_fold_result(gm, target_val, add.meta)
-                add.replace_all_uses_with(fold_res)
-                gm.graph.erase_node(add)
+                    fold_res = get_binary_fold_result(gm, target_node_cap.node, add_node_cap.node.meta)
+                add_node_cap.node.replace_all_uses_with(fold_res)
+                gm.graph.erase_node(add_node_cap.node)
 
         return changed
