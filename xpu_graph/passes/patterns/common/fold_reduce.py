67c8344849a4682922c0ca66b1e479fca44e2d56
lliinnkkkk <79913745@qq.com>
2025-08-21 21:45:46 +0800

    fix(fold_reduce): support for dim=none in fold reduce (#364) (#365)
    Co-authored-by: luohaizhao <luohaizhao@cambricon.com>

--- xpu_graph/passes/patterns/common/fold_reduce.py
+++ xpu_graph/passes/patterns/common/fold_reduce.py
@@ -1,6 +1,7 @@
+from typing import List
+
 import torch
 import torch.fx as fx
-from typing import List
 
 from xpu_graph.fx_utils import FxStage
 from xpu_graph.passes.patterns.pattern import Pattern
@@ -17,9 +18,7 @@ class FoldReduce(Pattern):
         FxStage.backward,
     ]
 
-    def _get_fold_result(
-        self, gm: fx.GraphModule, src, dims: List[int], keep_dim: bool
-    ) -> fx.Node:
+    def _get_fold_result(self, gm: fx.GraphModule, src, dims: List[int], keep_dim: bool) -> fx.Node:
         copy = gm.graph.call_function(
             torch.ops.aten._to_copy.default,
             args=(src,),
@@ -38,19 +37,16 @@ class FoldReduce(Pattern):
 
     def process(self, gm: fx.GraphModule):
         changed = False
-        candidates = [
-            node
-            for node in gm.graph.nodes
-            if node.op == "call_function" and node.target in reduce_tup
-        ]
+        candidates = [node for node in gm.graph.nodes if node.op == "call_function" and node.target in reduce_tup]
 
         for reduce in candidates:
             inp = get_input_node(reduce, 0)
-            dims = get_input_kw_node(reduce, "dim")
+            shape = inp.meta["val"].shape
+            dims = get_input_kw_node(reduce, "dim") or list(range(len(shape)))
             if not isinstance(dims, list):
                 dims = [dims]
             keep_dim = get_input_kw_node(reduce, "keepdim") or False
-            if all([inp.meta["val"].shape[dim] == 1 for dim in dims]):
+            if all([shape[dim] == 1 for dim in dims]):
                 changed = True
                 with gm.graph.inserting_before(reduce):
                     fold_res = self._get_fold_result(gm, inp, dims, keep_dim)
