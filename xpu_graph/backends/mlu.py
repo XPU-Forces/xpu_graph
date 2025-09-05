7f24041d3b777df69cedaa4fe2e3dadcf92e52b9
yfchen-byted <chenyifan.42@bytedance.com>
2025-09-05 12:15:12 +0800

    fix(cpp_wrapper_configs): update cpp_wrapper configs

--- xpu_graph/backends/mlu.py
+++ xpu_graph/backends/mlu.py
@@ -15,11 +15,10 @@ def mlu_compile(module: torch.nn.Module, example_inputs, **config_dict: Dict) ->
         module = decompose_for_inductor(module, example_inputs)
     logger.debug(f"After decompose_for_inductor, graph like:\n {module.graph}")
 
-    mode = config_dict.get("mode", "reduce-overhead")
-    cpp_wrapper = config_dict.get("cpp_wrapper", False)
     is_inference = config_dict.get("is_inference", False)
     is_backward = config_dict.get("is_backward", False)
 
+    mode = config_dict.get("mode", "reduce-overhead")
     if mode == "cudagraphs":
         from torch._dynamo.backends.cudagraphs import cudagraphs
 
@@ -30,20 +29,25 @@ def mlu_compile(module: torch.nn.Module, example_inputs, **config_dict: Dict) ->
 
     options = {}
     dynamic = config_dict.get("dynamic", True)
+
+    # Note(chenyifan): special handling of cpp_wrapper configs, this option is handled in compile_fx
+    #                  so we need to handle it here if we use compile_fx_inner directly
+    cpp_wrapper = config_dict.get("cpp_wrapper", torch._inductor.config.cpp_wrapper)
+
+    from packaging import version
+
+    torch_version = version.parse(torch.__version__[:5])
+    if cpp_wrapper and torch_version >= version.parse("2.7.0"):
+        from torch._inductor.compile_fx import get_cpp_wrapper_config
+
+        options.update(get_cpp_wrapper_config())
+    else:
+        options = {}
+
     inductor_backend = _TorchCompileInductorWrapper(mode, options, dynamic)
 
     with torch._inductor.config.patch(inductor_backend.config):
-        from packaging import version
-
-        torch_version = version.parse(torch.__version__[:5])
-        if cpp_wrapper and torch_version >= version.parse("2.7.0"):
-            from torch._inductor.compile_fx import get_cpp_wrapper_config
-
-            config = get_cpp_wrapper_config()
-        else:
-            config = {}
-        with torch._inductor.config.patch(**config):
-            compiled_func = compile_fx_inner(
-                module, example_inputs, cpp_wrapper=cpp_wrapper, is_inference=is_inference, is_backward=is_backward
-            )
+        compiled_func = compile_fx_inner(
+            module, example_inputs, cpp_wrapper=cpp_wrapper, is_inference=is_inference, is_backward=is_backward
+        )
     return compiled_func
