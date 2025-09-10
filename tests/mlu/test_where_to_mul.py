47312d0d6603cfe5bd1812e6a0d9d7c7b5aa0132
Yujiao Ji <a17621680413@163.com>
2025-09-10 18:19:43 +0800

    feat(mlu): add tmp

--- tests/mlu/test_where_to_mul.py
+++ tests/mlu/test_where_to_mul.py
@@ -28,6 +28,21 @@ def fn0(inputs, slice_, batch):
     return output, slice_1
 
 
+def fn1(inputs, slice_, batch):
+    zeros = torch.zeros([batch, 32], device=device, dtype=data_type)
+    slice_1 = slice_[:, 0:32]
+    slice_2 = slice_[:, 10118:10150]
+    slice_4 = slice_[:, 10579:10611]
+    slice_5 = slice_[:, 11032:11064]
+    slice_6 = slice_[:, 11445:11477]
+    where_0 = torch.where(inputs, slice_2, zeros)
+    where_1 = torch.where(inputs, slice_4, zeros)
+    where_2 = torch.where(inputs, slice_5, zeros)
+    where_3 = torch.where(inputs, slice_6, zeros)
+    output = torch.cat([slice_1, where_0, where_1, where_2, where_3], dim=-1)
+    return output, slice_1
+
+
 def where_to_mul_test(xpu_graph_backend, func):
     batch = 512
     random_list = random.choices([0, 1], k=batch)
@@ -49,6 +64,7 @@ class TestWhereToMul:
         "pattern_func",
         [
             fn0,
+            fn1,
         ],
     )
     def test_where_cat_patterns(self, caplog, pattern_func):
@@ -60,3 +76,4 @@ class TestWhereToMul:
 if __name__ == "__main__":
     xpu_graph_backend = xpu_graph.mlu_compiler(opt_level=OptLevel.level1, is_training=False, debug=True)
     where_to_mul_test(xpu_graph_backend, fn0)
+    where_to_mul_test(xpu_graph_backend, fn1)
--- xpu_graph/passes/patterns/targets/mlu/where_to_mul.py
+++ xpu_graph/passes/patterns/targets/mlu/where_to_mul.py
@@ -9,7 +9,45 @@ from xpu_graph.fx_utils import FxStage
 from xpu_graph.passes.patterns.pattern import Pattern
 from xpu_graph.utils import logger
 
-from ...utils.check_ops import check_where_op, check_zeros_op
+from ...utils.check_ops import check_where_op, is_one_like, is_zero_like
+
+
+def replace_where_to_mul(graph_module, nodes, cond, idx):
+    nodes_to_remove = []
+    for n in nodes:
+        with graph_module.graph.inserting_before(n):
+            target_node = n.args[idx]
+            cast_node = None
+            if hasattr(target_node, "meta") and "tensor_meta" in target_node.meta:
+                target_dtype = target_node.meta["tensor_meta"].dtype
+                if torch.bool != target_dtype:
+                    cast_node = graph_module.graph.create_node(
+                        op="call_function",
+                        target=torch.ops.aten._to_copy.default,
+                        args=(cond,),
+                        kwargs={"dtype": target_dtype},
+                        name=cond.name + "_cast",
+                    )
+            new_n = graph_module.graph.create_node(
+                op="call_function",
+                target=torch.ops.aten.mul.Tensor,
+                args=(cond if cast_node == None else cast_node, target_node),
+                name=n.name + "_tomul",
+            )
+            n.replace_all_uses_with(new_n)
+            nodes_to_remove.append(n)
+    for n in nodes_to_remove:
+        graph_module.graph.erase_node(n)
+
+
+def logical_not_cond(graph_module, cond):
+    new_cond = graph_module.graph.create_node(
+        op="call_function",
+        target=torch.ops.aten.logical_not.default,
+        args=(cond,),
+        name=cond.name + "_not",
+    )
+    return new_cond
 
 
 class FusedWhereToMul(Pattern):
@@ -25,38 +63,31 @@ class FusedWhereToMul(Pattern):
     def process(self, graph_module: fx.GraphModule) -> bool:
         is_modified = False
         candidates = [node for node in graph_module.graph.nodes if node.op == "call_function" and check_where_op(node)]
-        where_cond: Dict[fx.Node, List[fx.Node]] = {}
+        where_cond_z_l: Dict[fx.Node, List[fx.Node]] = {}
+        where_cond_z_r: Dict[fx.Node, List[fx.Node]] = {}
         for node in candidates:
             cond, x, y = node.args[:3]
-            if not check_zeros_op(x):
-                continue
-            if cond not in where_cond:
-                where_cond[cond] = []
-            where_cond[cond].append(node)
-        for cond, nodes in where_cond.items():
+            if is_zero_like(x):
+                if cond not in where_cond_z_l:
+                    where_cond_z_l[cond] = []
+                where_cond_z_l[cond].append(node)
+            elif is_zero_like(y):
+                if cond not in where_cond_z_r:
+                    where_cond_z_r[cond] = []
+                where_cond_z_r[cond].append(node)
+
+        for cond, nodes in where_cond_z_l.items():
             if len(nodes) < 3:
                 continue
-            # ~
             with graph_module.graph.inserting_after(cond):
-                new_cond = graph_module.graph.create_node(
-                    op="call_function",
-                    target=torch.ops.aten.logical_not.default,
-                    args=(cond,),
-                    name=cond.name + "_not",
-                )
-            # *
-            nodes_to_remove = []
-            for n in nodes:
-                with graph_module.graph.inserting_before(n):
-                    new_n = graph_module.graph.create_node(
-                        op="call_function",
-                        target=torch.ops.aten.mul.Tensor,
-                        args=(new_cond, n.args[2]),
-                        name=n.name + "_tomul",
-                    )
-                    n.replace_all_uses_with(new_n)
-                    nodes_to_remove.append(n)
-            for n in nodes_to_remove:
-                graph_module.graph.erase_node(n)
+                new_cond = logical_not_cond(graph_module, cond)
+                replace_where_to_mul(graph_module, nodes, new_cond, 2)
             is_modified = True
+
+        for cond, nodes in where_cond_z_r.items():
+            if len(nodes) < 2:
+                continue
+            replace_where_to_mul(graph_module, nodes, cond, 1)
+            is_modified = True
+
         return is_modified
