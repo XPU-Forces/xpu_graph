dd6c0cefae5f83b39ed937e234ddf6a60b90ed91
yfchen-byted <chenyifan.42@bytedance.com>
2025-10-30 12:05:07 +0800

    fix(fold_concrete_view): do not fold view if source view is concrete (#435)

--- src/xpu_graph/passes/patterns/common/fold_view.py
+++ src/xpu_graph/passes/patterns/common/fold_view.py
@@ -57,6 +57,38 @@ class FoldView1(Pattern):
 
     _support_stages = [FxStage.inference, FxStage.pregrad, FxStage.forward]
 
+    def _infer_folded_shape(self, orig_shape, new_shape):
+        if any(isinstance(dim, int) and dim < 0 for dim in orig_shape):
+            return new_shape
+        # Note: if orig_shape has no "-1", it actually performs a sym-shape assertions, and we should be careful when folding it
+        if any(isinstance(dim, int) and dim < 0 for dim in new_shape):
+            # The actual situation where new folded shape should be infered rather than naive folding
+            def _collec_mixed_numel(shape):
+                symbolic_dims = []
+                concrete_dims = 1
+                for dim in shape:
+                    if isinstance(dim, fx.Node):
+                        symbolic_dims.append(dim)
+                    elif dim > 0:
+                        concrete_dims *= dim
+                return sorted(symbolic_dims), concrete_dims
+
+            orig_symbolic_shapes, orig_concrete_shape = _collec_mixed_numel(orig_shape)
+            new_symbolic_shapes, new_concrete_shape = _collec_mixed_numel(new_shape)
+            if orig_symbolic_shapes != new_symbolic_shapes:
+                if orig_concrete_shape == new_concrete_shape:
+                    for orig_dim, new_dim in zip(orig_symbolic_shapes, new_symbolic_shapes + [None]):
+                        if orig_dim != new_dim:
+                            infered_dim = orig_dim
+                            break
+                else:
+                    # we give up to infer the folded shape if the symbolic part is different
+                    return None
+            else:
+                infered_dim = orig_concrete_shape // new_concrete_shape
+            return tuple(infered_dim if dim == -1 else dim for dim in new_shape)
+        return new_shape
+
     def process(self, gm: fx.GraphModule):
         changed = False
         view_tup = (
@@ -69,7 +101,12 @@ class FoldView1(Pattern):
         for view in candidates:
             inp = view.args[0]
             if isinstance(inp, fx.Node) and inp.op == "call_function" and inp.target in _view_like_ops:
-                changed = True
-                view.replace_input_with(inp, inp.args[0])
+                if inp.target in view_tup:
+                    if (folded_shape := self._infer_folded_shape(inp.args[1], view.args[1])) is not None:
+                        changed = True
+                        view.args = (inp.args[0], folded_shape)
+                else:
+                    changed = True
+                    view.replace_input_with(inp, inp.args[0])
 
         return changed
