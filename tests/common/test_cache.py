958ee49f563fb6fc73c96e9b804a50157ff7c62b
yfchen-byted <chenyifan.42@bytedance.com>
2025-11-11 20:46:00 +0800

    ci(mlu): only test picklizable infernce artifact for mlu

--- tests/common/test_cache.py
+++ tests/common/test_cache.py
@@ -1,7 +1,3 @@
-import itertools
-import os
-import pickle
-
 import torch
 
 from tests.common.test_models import SimpleModel, compare_inference
@@ -29,41 +25,6 @@ def test_xpugraph_cache(caplog, tmp_path):
 
     assert caplog.text.count("Save cache in location") == 2, "Should save cache twice"
     assert caplog.text.count("Use cache in location") == 2, "Should use cache twice"
-
-
-def test_xpugraph_inference_artifact(caplog, tmp_path):
-    infer_config = XpuGraphConfig(is_training=False, opt_level=OptLevel.level2, freeze=False)
-    infer_backend = XpuGraph(infer_config, XpuGraphLocalCache(tmp_path))
-
-    compiled_id = itertools.count()
-
-    def pkl_compiler(gm, example_inputs):
-        gm_id = next(compiled_id)
-        pkl_path = tmp_path / f"compiled_artifact_{gm_id}.pkl"
-
-        if os.path.exists(pkl_path):
-            with open(pkl_path, "rb") as f:
-                loaded_artifact = pickle.load(f)
-                return loaded_artifact
-
-        compiled_artifact = infer_backend(gm, example_inputs)
-        with open(pkl_path, "wb") as f:
-            pickle.dump(compiled_artifact, f)
-        return compiled_artifact
-
-    with need_xpu_graph_logs():
-        for bsz in [8, 10, 20]:
-            compare_inference(device, dtype, SimpleModel, pkl_compiler, bsz=bsz, dynamic=None)
-
-        # Mock a restart compilation process, but can access the pickled artifact
-        torch._dynamo.reset()
-        compiled_id = itertools.count()
-
-        for bsz in [8, 10, 20]:
-            compare_inference(device, dtype, SimpleModel, pkl_compiler, bsz=bsz, dynamic=None)
-
-    assert caplog.text.count("Save cache in location") == 2, "Should save cache twice"
-    assert caplog.text.count("Use cache in location") == 0, "Should directly use pickled artifact"
 
 
 if __name__ == "__main__":
