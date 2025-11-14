4dad2354196d7bffb8d589936179ee720cb3c99c
liuyuan.leo <liuyuan.leo@bytedance.com>
2025-11-14 17:48:37 +0800

    refactor(src/xpu_graph/cache.py): we should put all the related context for serializaiton together.

--- src/xpu_graph/cache.py
+++ src/xpu_graph/cache.py
@@ -4,6 +4,7 @@ import importlib
 import os
 import pickle
 from abc import ABC, abstractclassmethod, abstractmethod
+from functools import wraps
 from os import PathLike
 from typing import Tuple, Union
 
@@ -92,16 +93,27 @@ class SerializableArtifact(ABC):
         ...
 
 
+def make_it_real_mode(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        with compile_lock, _disable_current_modes(), TracingContext.patch(fake_mode=None):
+            return func(*args, **kwargs)
+
+    return wrapper
+
+
 class SerializableGraphModule(SerializableArtifact):
     def __init__(self, artifact):
         assert isinstance(artifact, GraphModule)
         super().__init__(artifact)
 
+    @make_it_real_mode
     def _serialize(self) -> Tuple:
         gm_dict, graph_meta, nodes_meta = GmSerializeHelper.serialize_fn(self._artifact)
         return (gm_dict, graph_meta, nodes_meta)
 
     @staticmethod
+    @make_it_real_mode
     def _deserialize(gm_dict, graph_meta, nodes_meta):
         gm = GmSerializeHelper.deserialize_fn(gm_dict, graph_meta, nodes_meta)
         return __class__(gm)
@@ -181,12 +193,14 @@ class SerializableCompiledFxGraph(SerializableArtifact):
         assert isinstance(artifact, CompiledFxGraph)
         super().__init__(artifact)
 
+    @make_it_real_mode
     def _serialize(self):
         mod = copy.copy(self._artifact)
         mod.current_callable = None
         return (mod,)
 
     @staticmethod
+    @make_it_real_mode
     def _deserialize(mod):
         logger.info(f"Deserializing a {CompiledFxGraph.__qualname__}")
         compiled_fn = mod
@@ -264,7 +278,6 @@ class XpuGraphLocalCache(XpuGraphCache):
 
         artifact_path = self._artifact_path(key)
         logger.info(f"Save cache in location: {artifact_path}")
-        # with compile_lock, _disable_current_modes(), TracingContext.patch(fake_mode=None):
         with open(artifact_path, "wb+") as f:
             pickle.dump(value, f)
         with open(artifact_path, "rb") as f:
@@ -273,7 +286,6 @@ class XpuGraphLocalCache(XpuGraphCache):
     def load_artifact(self, key):
         artifact_path = self._artifact_path(key)
         if os.path.isfile(artifact_path):
-            # with compile_lock, _disable_current_modes(), TracingContext.patch(fake_mode=None):
             logger.info(f"Use cache in location: {artifact_path}")
             with open(artifact_path, "rb") as f:
                 return pickle.load(f)
